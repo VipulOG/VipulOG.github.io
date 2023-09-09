27935042fe72e207f5b2d379113a6e8f3f0a12ec
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-09-09 05:26:11 +0000

    Added nav rail

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import NavRail from './components/NavRail.vue'
+
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const isWide = breakpoints.greater('md')
 </script>
 
 <template>
-  <header>
-    <NavBar />
-  </header>
-  <RouterView />
+  <div class="wrapper">
+    <header v-if="!isWide"><NavBar /></header>
+    <NavRail v-if="isWide" />
+    <main><RouterView /></main>
+  </div>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .wrapper {
+    display: flex;
+    height: 100vh;
+  }
+}
+</style>
---
+++ src/components/NavRail.vue
@@ -0,0 +1,63 @@
+<script setup>
+import HomeIcon from './icons/IconHome.vue'
+import ProfileIcon from './icons/IconProfile.vue'
+import ProjectIcon from './icons/IconProject.vue'
+import ArticleIcon from './icons/IconArticle.vue'
+import ContactIcon from './icons/IconContact.vue'
+</script>
+
+<template>
+  <nav>
+    <div class="nav-icon-container">
+      <RouterLink to="/">
+        <HomeIcon class="nav-icon" :class="{ selected: $route.name == 'home' }" />
+      </RouterLink>
+      <RouterLink to="/profile">
+        <ProfileIcon class="nav-icon" :class="{ selected: $route.name == 'profile' }" />
+      </RouterLink>
+      <RouterLink to="/works">
+        <ProjectIcon class="nav-icon" :class="{ selected: $route.name == 'works' }" />
+      </RouterLink>
+      <RouterLink to="/articles">
+        <ArticleIcon class="nav-icon" :class="{ selected: $route.name == 'articles' }" />
+      </RouterLink>
+    </div>
+    <button><ContactIcon style="width: 16px; height: 16px;" /></button>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  padding: 6px;
+  margin: 8px;
+  border-radius: 8px;
+  background-color: var(--md-sys-color-surface-variant);
+}
+
+.nav-icon-container {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.nav-icon {
+  width: 36px;
+  height: 36px;
+  padding: 6px;
+  color: var(--md-sys-color-on-surface);
+  border-radius: 20%;
+  transition:
+    color 0.5s ease,
+    background-color 0.5s ease,
+    transform 0.5s ease;
+}
+
+.nav-icon.selected {
+  color: var(--md-sys-color-on-primary);
+  background-color: var(--md-sys-color-primary);
+}
+</style>
