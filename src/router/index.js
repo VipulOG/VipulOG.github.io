4a83c61348bc7f6b048ab106950386d414432f31
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-09-09 04:09:40 +0000

    Add projects view

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
     {
       path: '/works',
       name: 'works',
-      component: () => import('../views/ProfileView.vue')
+      component: () => import('../views/ProjectsView.vue')
     }
   ]
 })
