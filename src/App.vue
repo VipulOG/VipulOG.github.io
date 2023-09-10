0590c255c4e649e59bfab5a1f7d99af415dd81fb
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-09-10 03:17:34 +0000

    Better wide layout handling

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,19 @@ import NavBar from '@/components/nav/NavBar.vue'
 import NavRail from '@/components/nav/NavRail.vue'
 
 import { useBreakpoints } from '@vueuse/core'
+import { provide } from 'vue'
 
 const wideBreakpoint = '768px'
 const breakpoints = useBreakpoints({ wideBreakpoint: wideBreakpoint })
-const isWide = breakpoints.greater('wideBreakpoint')
+const isWideDisplay = breakpoints.greater('wideBreakpoint')
+provide('isWideDisplay', isWideDisplay)
 </script>
 
 <template>
-  <div :class="{ 'wide-layout': isWide }">
-    <header v-if="!isWide"><NavBar style="margin: 8px" /></header>
-    <NavRail v-if="isWide" style="margin: 12px" />
-    <main style="flex: 1"><RouterView /></main>
+  <div :class="{ 'wide-layout': isWideDisplay }">
+    <header v-if="!isWideDisplay"><NavBar style="margin: 8px" /></header>
+    <NavRail v-if="isWideDisplay" style="margin: 12px" />
+    <main style="flex: 1; overflow-y: auto"><RouterView /></main>
   </div>
 </template>
 
@@ -22,5 +24,6 @@ const isWide = breakpoints.greater('wideBreakpoint')
 .wide-layout {
   display: flex;
   height: 100vh;
+  overflow: hidden;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,16 @@
 import ProfileSmall from '@/components/ProfileSmall.vue'
 import ProjectsSmall from '@/components/project/ProjectsSmall.vue'
 import BlogsSmall from '@/components/blog/BlogsSmall.vue'
+import { inject } from 'vue'
 
-import { useBreakpoints } from '@vueuse/core'
-
-const wideBreakpoint = '768px'
-const breakpoints = useBreakpoints({ wideBreakpoint: wideBreakpoint })
-const isWide = breakpoints.greater('wideBreakpoint')
+const isWideDisplay = inject('isWideDisplay')
 </script>
 
 <template>
-  <div class="home-view" :class="{ 'wide-view': isWide }">
-    <ProfileSmall class="item" :class="{ 'wide-item': isWide }" />
-    <ProjectsSmall class="item" :class="{ 'wide-item': isWide }" />
-    <BlogsSmall class="item" :class="{ 'wide-item': isWide }" />
+  <div class="home-view" :class="{ 'wide-view': isWideDisplay }">
+    <ProfileSmall class="item" :class="{ 'wide-item': isWideDisplay }" />
+    <ProjectsSmall class="item" :class="{ 'wide-item': isWideDisplay }" />
+    <BlogsSmall class="item" :class="{ 'wide-item': isWideDisplay }" />
   </div>
 </template>
 
