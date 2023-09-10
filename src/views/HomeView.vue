600292eb4385341d7331eb3f0e29d9bc50a709bd
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-09-10 06:52:08 +0000

    Rename ProfileSmall to MiniProfile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ProfileSmall from '@/components/ProfileSmall.vue'
+import MiniProfile from '@/components/profile/MiniProfile.vue'
 import ProjectsSmall from '@/components/project/ProjectsSmall.vue'
 import BlogsSmall from '@/components/blog/BlogsSmall.vue'
 import { inject } from 'vue'
@@ -10,7 +10,7 @@ const isWideDisplay = inject('isWideDisplay')
 <template>
   <div style="display: flex; justify-content: center">
     <div class="home-view" :class="{ 'wide-view': isWideDisplay }">
-      <ProfileSmall
+      <MiniProfile
         class="item"
         :class="{ 'wide-view-item': isWideDisplay }"
         style="grid-column: span 2"
