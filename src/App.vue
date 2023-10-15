126767e0d4d6f310f14e634454915bf6fbe2057c
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-10-15 14:05:09 +0000

    hide app nav on homescreen and add nav animations

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,158 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import NavBar from '@/components/nav/NavBar.vue'
-import NavRail from '@/components/nav/NavRail.vue'
-import BgParticles from '@/components/BgParticles.vue'
+import { RouterView, useRoute } from 'vue-router'
+import NavBar from './components/nav/NavBar.vue'
+import NavRail from './components/nav/NavRail.vue'
+import BgParticles from './components/BgParticles.vue'
 
-import { useBreakpoints } from '@vueuse/core'
-import { provide } from 'vue'
+import { computed, ref, watch } from 'vue'
 
-const wideBreakpoint = '768px'
-const breakpoints = useBreakpoints({ wideBreakpoint: wideBreakpoint })
-const isWideDisplay = breakpoints.greater('wideBreakpoint')
-provide('isWideDisplay', isWideDisplay)
+const route = useRoute()
+const isFirstRoute = ref(true)
+const isCurrentRouteHome = computed(() => route.name === 'home')
+
+watch(
+  () => route.path,
+  () => {
+    if (isFirstRoute.value) isFirstRoute.value = false
+  }
+)
+
+const navHide = computed(() => {
+  return [
+    { hide: isFirstRoute.value },
+    { 'hide-anim': isCurrentRouteHome.value && !isFirstRoute.value }
+  ]
+})
 </script>
 
 <template>
-  <div :class="['flex', isWideDisplay ? 'row' : 'col']">
-    <NavBar v-if="!isWideDisplay" style="margin: 8px" />
-    <NavRail v-else style="margin: 12px" />
-    <main style="flex: 1; overflow-y: auto">
-      <RouterView />
+  <div class="wrapper">
+    <NavBar class="nav-bar" :class="navHide" />
+    <NavRail class="nav-rail" :class="navHide" />
+    <main>
+      <router-view v-slot="{ Component }">
+        <transition :name="route.meta.transition || 'fade'" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
     </main>
     <BgParticles></BgParticles>
   </div>
 </template>
 
 <style scoped>
-.flex {
+.wrapper {
   display: flex;
   height: 100vh;
-}
-
-.row {
   flex-direction: row;
 }
 
-.col {
-  flex-direction: column;
+main {
+  flex: 1;
+  overflow-y: auto;
+  height: 100%;
+}
+
+.nav-bar {
+  display: none;
+  animation: 0.3s ease slideInFromTop;
+  margin: 8px;
+  width: calc(100% - 16px);
+}
+
+.nav-bar.hide {
+  display: none;
+}
+
+.nav-bar.hide-anim {
+  animation: 0.3s ease slideOutToTop;
+  animation-fill-mode: forwards;
+}
+
+.nav-rail {
+  display: block;
+  animation: 0.3s ease slideInFromLeft;
+  margin: 12px;
+  height: calc(100% - 24px);
+}
+
+.nav-rail.hide {
+  display: none;
+}
+
+.nav-rail.hide-anim {
+  animation: 0.3s ease slideOutToLeft;
+  animation-fill-mode: forwards;
+}
+
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all 0.5s;
+}
+
+.slide-right-enter,
+.slide-right-leave-to {
+  transform: translateX(100%);
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+@media (max-width: 767px) {
+  .wrapper {
+    flex-direction: column;
+  }
+  .nav-bar {
+    display: block;
+  }
+  .nav-rail {
+    display: none;
+  }
+}
+
+@keyframes slideInFromLeft {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideOutToLeft {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+    display: none;
+  }
+}
+
+@keyframes slideInFromTop {
+  0% {
+    transform: translateY(-100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slideOutToTop {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-100%);
+    display: none;
+  }
 }
 </style>
