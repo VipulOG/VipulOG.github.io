c2c99ec026753f739a615dc6ca6e6110e8ce48cf
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-10-15 13:55:21 +0000

    add navbar in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,31 @@
 <script setup>
+import NavBar from '../components/nav/NavBar.vue'
 import MiniProfile from '../components/MiniProfile.vue'
-
-
 </script>
 
 <template>
-  <div style="height: 100%; display: flex; align-items: center; justify-content: center;">
-    <MiniProfile class="outline" style="max-width: 600px; margin: 12px;" />
+  <div class="wrapper">
+    <MiniProfile class="outline mini-profile" />
+    <NavBar class="navbar" />
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.mini-profile {
+  margin: 12px;
+  max-width: 600px;
+}
+
+.navbar {
+  max-width: 460px;
+  width: calc(100% - 24px);
+}
+</style>
