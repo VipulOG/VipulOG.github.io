0eca9618fe577577d7db6bce58f37a09507993ee
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2023-10-16 12:05:10 +0000

    display "InProgressView" for all wip pages

---
+++ src/views/InProgressView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <h1>WIP</h1>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
